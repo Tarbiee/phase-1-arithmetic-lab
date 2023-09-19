3c9982fc5a9a6ec9a4edebdfc7012025c366adc3
Tarbiee <dorcusstaby@gmail.com>
2023-09-19 15:45:04 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+console.log(multiply);
+
+
+let random= Math.floor(Math.random() *10) +1;
+
+const num3 =29;
+const num4 = 5;
+const mod = num3 % num4;
+console.log(mod);
+
+
+const max  = Math.max(18,17,5,20);
